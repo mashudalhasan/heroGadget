131ab0cc0104ff314db8492bde079f29a5886dd8
mashudalhasan <mashudalhasan554@gmail.com>
2023-04-09 16:09:58 +0600

    added dynamic shop page

---
+++ src/components/Shop.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import ProductCard from "./Cards/ProductCard";
+
+const Shop = () => {
+  const productData = useLoaderData();
+
+  // card button handler
+  const handleAddToCart = (id) => {
+    console.log(id);
+  };
+
+  return (
+    <div className="my-container">
+      <div className="product-container">
+        {productData.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></ProductCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import About from './components/About';
-import Home from './components/Home';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import ErrorPage from "./components/ErrorPage";
+import Shop from "./components/Shop";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App></App>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: '/about',
-                element: <About></About>
-            }
-        ]
-    },
-])
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/shop",
+        element: <Shop></Shop>,
+        loader: () => fetch("products.json"),
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}></RouterProvider>)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
