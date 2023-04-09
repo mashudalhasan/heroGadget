0b802c8e0addc68eeceb2869fdf800b3e7b439ca
mashudalhasan <mashudalhasan554@gmail.com>
2023-04-09 13:06:10 +0600

    implemented dynamic page router

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App></App>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/about',
+                element: <About></About>
+            }
+        ]
+    },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}></RouterProvider>)
